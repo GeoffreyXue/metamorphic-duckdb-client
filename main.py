ae49bec91c44cf6d8097ba7bcfb4b074c04ec6cf
GeoffreyXue <xuegeoff@gmail.com>
2023-12-11 20:21:47 -0600

    Add additional environment config, tested and functional

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import os
 
 load_dotenv()
 
-region = 'us-east-1'
+region = os.environ.get("AWS_REGION")
+s3_bucket = os.environ.get("AWS_S3_BUCKET")
+
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-s3_bucket = os.environ.get("S3_BUCKET")
 
-con = duckdb.connect(database=':memory:', read_only=True)
+
+con = duckdb.connect(database=':memory:')
 
 # Install and load the httpfs extension (run once)
 install_httpfs_query = 'INSTALL httpfs;'
@@ -26,7 +28,7 @@ con.execute(s3_access_key_id_query)
 con.execute(s3_secret_access_key_query)
 
 # Create a view for all of the parquet files in the bucket
-view_query = f"CREATE VIEW data AS SELECT * FROM s3://{s3_bucket}/*.parquet;"
+view_query = f"CREATE VIEW data AS SELECT * FROM read_parquet('s3://{s3_bucket}/*.parquet');"
 con.execute(view_query)
 
 # Query the data
